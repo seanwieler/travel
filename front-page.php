2f950ec3d22584cb7bdd33d6e9158c407727ef6a
Sean Wieler <sbwieler@gmail.com>
2018-10-01 13:47:59 -0700

    adding background to recent posts

--- front-page.php
+++ front-page.php
@@ -30,11 +30,12 @@ get_header();
 				echo wp_get_attachment_image( $image, $size );
 			
 			}*/
+			
+			<?php get_template_part( 'template-parts/content', 'page' ); ?> 
 			echo get_post_meta(get_the_ID(), "hero_quote", true);?>
 			<h2 class="decorated"><span><?php echo get_post_meta(get_the_ID(), "section_1_title", true);?></span></h2> 
 			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_1_content", true); ?> </div> 
 			<h2 class="decorated"><span><?php echo get_post_meta(get_the_ID(), "recent_posts_title", true);?></span></h2> 
-			<?php get_template_part( 'template-parts/content', 'page' ); ?> 
 			<h2 class="decorated"><span><?php echo get_post_meta(get_the_ID(), "section_2_title", true);?></span></h2> 
 			<?php photo_gallery(1); ?>
 			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_2_content", true); ?> </div> <?php
