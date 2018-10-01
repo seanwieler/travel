de6de885a22fd3ef892da3b573bccd625781d7f5
Sean Wieler <sbwieler@gmail.com>
2018-09-30 22:22:25 -0700

    added section 2

--- front-page.php
+++ front-page.php
@@ -33,9 +33,9 @@ get_header();
 			// echo get_post_meta(get_the_ID(), "hero_image", true);
 			echo get_post_meta(get_the_ID(), "hero_quote", true);?>
 			<h2 class="decorated"><span><?php echo get_post_meta(get_the_ID(), "section_1_title", true);?></span></h2> 
-			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_1_content", true); ?> </div> 
-			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_2_content", true); ?> </div><?php
-			get_template_part( 'template-parts/content', 'page' );
+			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_1_content", true); ?> </div> <?php
+			get_template_part( 'template-parts/content', 'page' ); ?>
+			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_2_content", true); ?> </div> <?php
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
