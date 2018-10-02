8b8781b72fde0e51236b07d3e5435c56211b570d
Sean Wieler <sbwieler@gmail.com>
2018-10-01 17:08:14 -0700

    recent posts test

--- front-page.php
+++ front-page.php
@@ -21,14 +21,10 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
 			$image = get_field('hero_image');
 			$size = '50%'; // (thumbnail, medium, large, full or custom size)
-			
 			if( $image ) {
-			
 				echo wp_get_attachment_image( $image, $size );
-			
 			}
 			?>
 			<h2 class="decorated black-bg"><span><?php echo get_post_meta(get_the_ID(), "section_1_title", true);?></span></h2> 
@@ -36,7 +32,12 @@ get_header();
 			<div class="rp-background">
 				<h2 class="decorated white-bg"><span><?php echo get_post_meta(get_the_ID(), "recent_posts_title", true);?></span></h2> 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-			</div>
+			</div> <?php
+			$image = get_field('section_2_image');
+			$size = '50%'; // (thumbnail, medium, large, full or custom size)
+			if( $image ) {
+				echo wp_get_attachment_image( $image, $size );
+			} ?>
 			<h2 class="decorated black-bg"><span><?php echo get_post_meta(get_the_ID(), "section_2_title", true);?></span></h2> 
 				<?php photo_gallery(1); ?>
 			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_2_content", true); ?> </div> <?php
