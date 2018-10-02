efedc0f15b70effab4802317d4a6d8330ae070b4
Sean Wieler <sbwieler@gmail.com>
2018-10-01 18:00:44 -0700

    added image

--- front-page.php
+++ front-page.php
@@ -40,7 +40,11 @@ get_header();
 			} ?>
 			<h2 class="decorated black-bg"><span><?php echo get_post_meta(get_the_ID(), "section_2_title", true);?></span></h2> 
 				<?php photo_gallery(1); ?>
-			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_2_content", true); ?> </div> <?php
+			<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_2_content", true); ?> </div>
+			<div class="rp-background">
+				<h2 class="decorated white-bg"><span><?php echo get_post_meta(get_the_ID(), "section_3_title", true);?></span></h2> 
+				<div class="section-one-content"> <?php echo get_post_meta(get_the_ID(), "section_3_content", true); ?> </div>
+			</div> <?php
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
