c5606157e60c7a0995f3f103bcecd955336a2ed3
Sean Wieler <sbwieler@gmail.com>
2018-10-04 16:35:14 -0700

    created secondary page template

--- secondary-pages.php
+++ secondary-pages.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Front Page
+ * Template Name: General Page
  * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
@@ -53,8 +53,6 @@ get_header();
 			<?php if(get_field('section_2_title')): ?>
 			<h2 class="decorated black-bg"><span><?php echo get_post_meta(get_the_ID(), "section_2_title", true);?></span></h2>
 			<?php endif;?>
-
-			<?php photo_gallery(1); ?>
 
 			<?php if(get_field('section_2_content')): ?>
 				<div class="section-content-blackbg"> <?php the_field('section_2_content'); ?> </div>
