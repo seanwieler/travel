9bb4286fae6938d51573ea4b51b67bd2c8bb38df
Sean Wieler <sbwieler@gmail.com>
2018-10-04 15:04:33 -0700

    added widget support to footer

--- footer.php
+++ footer.php
@@ -14,6 +14,29 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+	<div id="footer-sidebar" class="secondary">
+		<div id="footer-sidebar1">
+			<?php
+			if(is_active_sidebar('footer-sidebar-1')){
+			dynamic_sidebar('footer-sidebar-1');
+			}
+			?>
+		</div>
+		<div id="footer-sidebar2">
+				<?php
+				if(is_active_sidebar('footer-sidebar-2')){
+				dynamic_sidebar('footer-sidebar-2');
+				}
+				?>
+		</div>
+		<div id="footer-sidebar3">
+			<?php
+			if(is_active_sidebar('footer-sidebar-3')){
+			dynamic_sidebar('footer-sidebar-3');
+			}
+			?>
+			</div>
+		</div>
 		<div class="site-info foot-text-color">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'Travel' ) ); ?>">
 				<?php
