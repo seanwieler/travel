ab1d13916d916d958d8dfce58d6f32803124347a
Sean Wieler <sbwieler@gmail.com>
2018-10-04 21:51:09 -0700

    created contact form

--- contact-page.php
+++ contact-page.php
@@ -37,41 +37,7 @@ get_header();
 			    <div class="section-content-blackbg"> <?php echo get_post_meta(get_the_ID(), "section_1_content", true); ?> </div> 
 			<?php endif;
 			
-			$image2 = get_field('section_2_image');
-			$size2 = '50%'; // (thumbnail, medium, large, full or custom size)
-			if( $image2 ) {
-				echo wp_get_attachment_image( $image2, $size2 );
-            } ?>
-
-            <?php if(get_field('section_2_title')): ?>
-                <div class="rp-background">
-                    <h2 class="decorated white-bg"><span><?php echo get_post_meta(get_the_ID(), "section_2_title", true);?></span></h2>
-                    <?php if(get_field('section_2_content')): ?>
-                        <div class="section-content-whitebg"> <?php the_field('section_2_content'); ?> </div>
-                    <?php endif;?>
-                </div> 
-        <?php endif; ?>
-			
-			<?php if(get_field('section_3_title')): ?>
-			<div class="rp-background">
-				<h2 class="decorated white-bg"><span><?php echo get_post_meta(get_the_ID(), "section_3_title", true);?></span></h2> 
-			<?php endif; ?>
-
-			<?php if(get_field('section_3_content')): ?>
-				<div class="section-content-whitebg"> <?php echo get_post_meta(get_the_ID(), "section_3_content", true); ?> </div>
-			</div> 
-			<?php endif;
-
-
-			$image3 = get_field('contact_image');
-			$size3 = '50%'; // (thumbnail, medium, large, full or custom size)
-			if( $image3 ) {
-				echo wp_get_attachment_image( $image3, $size3 );
-			}?>
-
-			<?php if(get_field('contact_content')): ?>
-			<div class="section-content-blackbg"> <?php echo get_post_meta(get_the_ID(), "contact_content", true); ?> </div> 
-			<?php endif;
+			echo do_shortcode("[contact-form-7 id="249" title="Contact Me Form"]");
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
