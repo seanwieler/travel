464598edb7991ca06b14d7d6dfce88f5efaea1ce
Sean Wieler <sbwieler@gmail.com>
2018-10-07 15:04:23 -0700

    rework menu

--- js/navigation.js
+++ js/navigation.js
@@ -104,4 +104,13 @@
 		}
 	}( container ) );
 } )();
-
+jQuery(document).ready(function($) {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) { 
+            $('header').addClass('shrink');
+        }
+        else{
+            $('header').removeClass('shrink');
+        }
+    });
+});
