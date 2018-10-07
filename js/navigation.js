8cac498f42f82f7982e2315cab32ed3057ef2a8d
Sean Wieler <sbwieler@gmail.com>
2018-10-07 14:36:45 -0700

    rework menu

--- js/navigation.js
+++ js/navigation.js
@@ -104,3 +104,15 @@
 		}
 	}( container ) );
 } )();
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("wp-megamenu-menu-1").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("wp-megamenu-menu-1").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
