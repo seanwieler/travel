55ddfecdee060ff73484bda40d8631877417d400
Sean Wieler <sbwieler@gmail.com>
2018-10-08 20:51:04 -0700

    tried recent post widget shortcode

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 	<div id="fixedbar">
 		<div class="boxfloat">
 			<ul id="tips">
-				<li><a href="<?php echo do_shortcode('[do_widget id=recent-posts-2]')?>">WPBeginner Link is the First Item</a></li>
+				<li><a><?php echo do_shortcode('[do_widget id=recent-posts-2]')?></a></li>
 				<li><a href="http://www.wordpress.org/">WordPress.org is the Second Item</a></li>
 			</ul>
 		</div>
