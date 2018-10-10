e52e45852232a9c18e7c8e10a4faf7779c7e0979
Sean Wieler <sbwieler@gmail.com>
2018-10-09 20:24:51 -0700

    removed footer widget from phone

--- front-page.php
+++ front-page.php
@@ -31,12 +31,6 @@ get_header();
 			$size = '50%'; // (thumbnail, medium, large, full or custom size)
 			if( $image ) {
 				echo wp_get_attachment_image( $image, $size );
-			}
-
-			$image = get_field('hero_image');
-			$size = '50%'; // (thumbnail, medium, large, full or custom size)
-			if( $image ) {
-				echo wp_get_attachment_image( $image, $size );
 			}?>
 
 			<?php if(get_field('section_1_title')): ?>
