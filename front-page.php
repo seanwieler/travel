6911238ebeb385fae317081bee0a622297e4df13
Sean Wieler <sbwieler@gmail.com>
2018-10-10 20:34:47 -0700

    changed hero styles

--- front-page.php
+++ front-page.php
@@ -45,14 +45,10 @@ get_header();
 			</div> 
 			<?php endif;
 
-			if(is_front_page()) {?>
-			<div class="desktop-hero"><?php echo do_shortcode('[parallax-scroll id="384"]');?></div><?php
-			}
-
 			$image2 = get_field('section_2_image');
 			$size2 = '50%'; // (thumbnail, medium, large, full or custom size)
 			if( $image2 ) {?>
-				<div class="mobile-hero"><?php echo wp_get_attachment_image( $image, $size );?></div><?php
+				<div><?php echo wp_get_attachment_image( $image, $size );?></div><?php
 			} ?>
 
 			<?php if(get_field('section_2_title')): ?>
@@ -77,10 +73,6 @@ get_header();
 			</div> 
 			<?php endif;
 
-			if(is_front_page()) { ?>
-				<div class="desktop-hero"><?php echo do_shortcode('[parallax-scroll id="386"]');?></div><?php
-			}
-			
 			$image3 = get_field('contact_image');
 			$size3 = '50%'; // (thumbnail, medium, large, full or custom size)
 			if( $image3 ) {
