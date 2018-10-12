db22a1b93b101a81b66cc64935d00e7633497031
Sean Wieler <sbwieler@gmail.com>
2018-10-11 17:34:05 -0700

    mobile setup content

--- front-page.php
+++ front-page.php
@@ -79,6 +79,10 @@ get_header();
 				echo wp_get_attachment_image( $image3, $size3 );
 			}
 
+			<?php if(get_field('contact_title')): ?>
+			<div class="decorated black-bg"><span><?php echo get_post_meta(get_the_ID(), "contact_title", true);?></span></div>
+			<?php endif;
+
 			if(get_field('contact_content')): ?>
 			<div class="section-content-blackbg"> <?php echo get_post_meta(get_the_ID(), "contact_content", true); ?> </div> 
 			<?php endif;
